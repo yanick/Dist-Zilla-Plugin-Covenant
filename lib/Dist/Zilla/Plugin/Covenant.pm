32a6b5ec711daba69ffdb7ed733c40df8217c8bb
Yanick Champoux <yanick@babyl.dyndns.org>
2011-11-17 19:38:24 -0500

    initial work

--- lib/Dist/Zilla/Plugin/Covenant.pm
+++ lib/Dist/Zilla/Plugin/Covenant.pm
@@ -1,5 +1,39 @@
 package Dist::Zilla::Plugin::Covenant;
-# add the author's pledge to the distribution
+# ABSTRACT: add the author's pledge to the distribution
+
+=head1 SYNOPSIS
+
+In dist.ini:
+
+    [Covenant]
+    version = 1
+    pledge_file = AUTHOR_PLEDGE
+
+=head1 DESCRIPTION
+
+C<Dist::Zilla::Plugin::Covenant> adds the file
+'I<AUTHOR_PLEDGE>' to the distribution. The author
+as defined in I<dist.ini> is taken as being the pledgee.
+
+The I<META> file of the distribution is also modified to
+include a I<x_author_pledge> stanza.
+
+=head1 CONFIGURATION OPTIONS
+
+=head2 version
+
+Version of the pledge to use. 
+
+Defaults to '1'.
+
+=head2 pledge_file
+
+Name of the file holding the pledge.
+
+Defaults to 'AUTHOR_PLEDGE'.
+
+=cut
+
 
 use strict;
 use warnings;
@@ -10,23 +44,31 @@ use Dist::Zilla::File::InMemory;
 with 'Dist::Zilla::Role::InstallTool';
 with 'Dist::Zilla::Role::TextTemplate';
 
+has pledge_file => (
+    is => 'ro',
+    default => 'AUTHOR_PLEDGE',
+);
+
 sub setup_installer {
     my $self = shift;
-    my $manual_installation;
-    (my $main_package = $self->zilla->name) =~ s!-!::!g;
+
+    $self->zilla->distmeta->{x_author_pledge} = {
+        version => 1,
+    };
 
     my $pledge = $self->fill_in_string(
         pledge_template(), {   
             distribution        => $self->zilla->name,
-            author => $self->zilla->author,
+            author => join( ', ', @{ $self->zilla->authors } ),
         }
     );
 
     my $file = Dist::Zilla::File::InMemory->new({ 
             content => $pledge,
-            name    => 'AUTHOR_PLEDGE',
+            name    => $self->pledge_file,
         }
     );
+
     $self->add_file($file);
 }
 
@@ -35,7 +77,7 @@ sub pledge_template {
 
 # CPAN Covenant for {{ $distribution }}
 
-I, {{ $author }}, hereby give modules@perl.org permission to grant co-maintainership
+I, {{ $author }}, hereby give modules@perl.org permission to grant co-maintainership 
 to {{ $distribution }}, if all the following conditions are met:
 
    (1) I haven't released the module for a year or more
